caf805f9108ec52f64a7271185e057852abaaac6
Anushka Singh <me.anushkasingh8971@gmail.com>
2025-03-21 22:41:27 +0530

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div classname='text-3xl'>Hackerzstreet3.0</div>
+        </>
+  )
+}
+
+export default App
