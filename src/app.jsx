341195f076a01a95879429d0a3e9062e2a8e1482
Ayush Ranjan <ayush82194@gmail.com>
2025-03-31 06:28:28 +0530

    Tracks Page updated (only styling)

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,8 +6,25 @@ function App() {
 
   return (
     <>
-      <div classname='text-3xl'>Hackerzstreet3.0</div>
-        </>
+      <article className='article-background'>
+        <h1>TRACKS</h1>
+        
+        <div class="track_container">
+          <h2>Choose any one from the following themes for your project</h2>
+          
+          <div class="track_grid">
+            <div class="track_item"><img src="/tracks_logo_1.svg" alt="tracks_Logo__1" width="150"></img></div>
+            <div class="track_item"><img src="/tracks_logo_2.svg" alt="tracks_Logo__2" width="150"></img></div>
+            <div class="track_item"><img src="/tracks_logo_3.svg" alt="tracks_Logo__3" width="150"></img></div>
+            <div class="track_item"><img src="/tracks_logo_4.svg" alt="tracks_Logo__4" width="150"></img></div>
+            <div class="track_item"><img src="/tracks_logo_5.svg" alt="tracks_Logo__5" width="150"></img></div>
+            <div class="track_item"><img src="/tracks_logo_6.svg" alt="tracks_Logo__6" width="150"></img></div>
+          </div>
+
+        </div>
+      </article>
+
+    </>
   )
 }
 
