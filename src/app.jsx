e83b2d1db7eef13b314e0a2c2d6d0af68058a099
Ayush Ranjan <ayush82194@gmail.com>
2025-03-31 16:56:32 +0530

    Added hover animation

--- src/app.jsx
+++ src/app.jsx
@@ -13,14 +13,38 @@ function App() {
           <h2>Choose any one from the following themes for your project</h2>
           
           <div class="track_grid">
-            <div class="track_item"><img src="/tracks_logo_1.svg" alt="tracks_Logo__1" width="150"></img></div>
-            <div class="track_item"><img src="/tracks_logo_2.svg" alt="tracks_Logo__2" width="150"></img></div>
-            <div class="track_item"><img src="/tracks_logo_3.svg" alt="tracks_Logo__3" width="150"></img></div>
-            <div class="track_item"><img src="/tracks_logo_4.svg" alt="tracks_Logo__4" width="150"></img></div>
-            <div class="track_item"><img src="/tracks_logo_5.svg" alt="tracks_Logo__5" width="150"></img></div>
-            <div class="track_item"><img src="/tracks_logo_6.svg" alt="tracks_Logo__6" width="150"></img></div>
-          </div>
 
+            <div class="track_item">
+              <img src="/tracks_logo_4.svg" alt="FinTech" width="100" height="100"></img>
+              <div class="hidden"><p class="track_subject">FinTech</p>Innovate in financial services with secure, scalable, and user-friendly solutions.</div>
+            </div>
+
+            <div class="track_item">
+              <img src="/tracks_logo_6.svg" alt="Healthcare" width="100" height="100"></img>
+              <div class="hidden"><p class="track_subject">Healthcare</p>Develop solutions to enhance patient care, diagnostics, and medical accessibility.</div>
+            </div>
+
+            <div class="track_item">
+              <img src="/tracks_logo_5.svg" alt="EdTech" width="100" height="100"></img>
+              <div class="hidden"><p class="track_subject">EdTech</p>Transform learning experiences through technology-driven educational advancements.</div>
+            </div>
+
+            <div class="track_item">
+              <img src="/tracks_logo_1.svg" alt="Blockchain" width="100" height="100"></img>
+              <div class="hidden"><p class="track_subject">Blockchain</p>Leverage decentralized technologies for transparency, security, and trust.</div>
+            </div>
+
+            <div class="track_item">
+              <img src="/tracks_logo_2.svg" alt="Environmental Impact" width="100" height="100"></img>
+              <div class="hidden"><p class="track_subject">Environmental Impact</p>Build tech-driven solutions to tackle sustainability and climate challenges.</div>
+            </div>
+
+            <div class="track_item">
+              <img src="/tracks_logo_3.svg" alt="Networking and Communication" width="100" height="100"></img>
+              <div class="hidden"><p class="track_subject">Networking and Communication</p>Enhance global connectivity through efficient and seamless communication systems.</div>
+            </div>
+
+          </div>
         </div>
       </article>
 
