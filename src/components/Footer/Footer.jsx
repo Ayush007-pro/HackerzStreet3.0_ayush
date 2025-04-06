12954e4af13b02f83c062ceb0ab9fdc8ab026892
Ayush Ranjan <ayush82194@gmail.com>
2025-04-06 21:14:46 +0530

    footer is made responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,44 +1,89 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Footer.css';
 
-export default function Footer() {
+export default function Footer({ contacts, socialLinks }) {
+  const defaultContacts = [
+    { name: 'Rishi Joshi', phone: '+919569913103', role: 'Chairperson' },
+    { name: 'Pranav Bhardwaj', phone: '+919999558266', role: 'Vice-Chairperson' },
+    { name: 'Mohit Hingorani', phone: '+919811033533', role: 'General Secretary' },
+  ];
+
+  const defaultSocialLinks = [
+    { href: 'https://www.instagram.com/ieee_csmuj/', src: '/social_media/instagram.svg', alt: 'Instagram' },
+    { href: 'https://www.linkedin.com/company/ieee-cs-muj', src: '/social_media/linkedin.svg', alt: 'LinkedIn' },
+    { href: 'https://medium.com/@ieeecs', src: '/social_media/medium.svg', alt: 'Medium' },
+    { href: 'https://x.com/ieeecsmuj', src: '/social_media/twitter.svg', alt: 'Twitter' },
+  ];
+
+  const contactList = contacts || defaultContacts;
+  const socialMediaList = socialLinks || defaultSocialLinks;
+
   return (
-    <footer id='contact'>
-      <div class='footer_logo'>
-        <img src='/footer/footer_logo.svg'/>
+    <footer id="contact">
+      <div className="footer_logo">
+        <img src="/footer/footer_logo.svg" alt="Footer Logo" />
         <div>/</div>
-        <img src='/footer/ieee_main_logo_large.svg'/>
+        <img src="/footer/ieee_main_logo_large.svg" alt="IEEE CS Logo" />
       </div>
-      <div class='footer_contacts'>
-        <div>
-          <span>Rishi Joshi</span>
-          <a href='tel:+919569913103'><p>+91 95699 13103</p></a>
-          <p>Chairperson</p>
-        </div>
-        <div>
-          <span>Pranav Bhardwaj</span>
-          <a href='tel:+919999558266'><p>+91 99995 58266</p></a>
-          <p>Vice-Chairperson</p>
-        </div>
-        <div>
-          <span>Mohit Hingorani</span>
-          <a href='tel:+919811033533'><p>+91 98110 33533</p></a>
-          <p>General Secretary</p>
-        </div>
+      <div className="footer_contacts">
+        {contactList.map((contact, index) => (
+          <div key={index}>
+            <span>{contact.name}</span>
+            <a href={`tel:${contact.phone}`}>
+              <p>{contact.phone}</p>
+            </a>
+            <p>{contact.role}</p>
+          </div>
+        ))}
       </div>
-      <div class='social_media'>
-        <div class='links'>
-          <a href='https://cs.ieeemuj.com/'><img src='/social_media/globe.svg'/></a>
-          <a href='https://cs.ieeemuj.com/'>https://cs.ieeemuj.com/</a>
+      <div className="social_media">
+        <div className="links">
+          <a href="https://cs.ieeemuj.com/">
+            <img src="/social_media/globe.svg" alt="Website" />
+          </a>
+          <a href="https://cs.ieeemuj.com/">https://cs.ieeemuj.com/</a>
         </div>
-        <div class='media_icons'>
-          <a href='https://www.instagram.com/ieee_csmuj/'><img src='/social_media/instagram.svg'/></a>
-          <a href='https://www.linkedin.com/company/ieee-cs-muj'><img src='/social_media/linkedin.svg'/></a>
-          <a href='https://medium.com/@ieeecs'><img src='/social_media/medium.svg'/></a>
-          <a href='https://x.com/ieeecsmuj'><img src='/social_media/twitter.svg'/></a>
+        <div className="media_icons">
+          {socialMediaList.map((link, index) => (
+            <a key={index} href={link.href}>
+              <img src={link.src} alt={link.alt} />
+            </a>
+          ))}
         </div>
-        <div class='tagline'>Made with 💛 by IEEE CS MUJ</div>
+        <div className="tagline">Made with 💛 by IEEE CS MUJ</div>
       </div>
     </footer>
-  )
+  );
 }
+
+Footer.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      phone: PropTypes.string.isRequired,
+      role: PropTypes.string.isRequired,
+    })
+  ),
+  socialLinks: PropTypes.arrayOf(
+    PropTypes.shape({
+      href: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+      alt: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+Footer.defaultProps = {
+  contacts: [
+    { name: 'Rishi Joshi', phone: '+919569913103', role: 'Chairperson' },
+    { name: 'Pranav Bhardwaj', phone: '+919999558266', role: 'Vice-Chairperson' },
+    { name: 'Mohit Hingorani', phone: '+919811033533', role: 'General Secretary' },
+  ],
+  socialLinks: [
+    { href: 'https://www.instagram.com/ieee_csmuj/', src: '/social_media/instagram.svg', alt: 'Instagram' },
+    { href: 'https://www.linkedin.com/company/ieee-cs-muj', src: '/social_media/linkedin.svg', alt: 'LinkedIn' },
+    { href: 'https://medium.com/@ieeecs', src: '/social_media/medium.svg', alt: 'Medium' },
+    { href: 'https://x.com/ieeecsmuj', src: '/social_media/twitter.svg', alt: 'Twitter' },
+  ],
+};
