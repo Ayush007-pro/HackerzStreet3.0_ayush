23fc1643e1a17add74ac64a689562e7300cf9f0e
Ayush Ranjan <ayush82194@gmail.com>
2025-04-06 11:52:06 +0530

    Home Page made responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,60 +1,85 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Home.css';
 
 export default function Home() {
+  const socialLinks = [
+    { href: 'https://www.instagram.com/ieee_csmuj/', src: '/social_media/instagram.svg', alt: 'Instagram' },
+    { href: 'https://www.linkedin.com/company/ieee-cs-muj', src: '/social_media/linkedin.svg', alt: 'LinkedIn' },
+    { href: 'https://medium.com/@ieeecs', src: '/social_media/medium.svg', alt: 'Medium' },
+    { href: 'https://x.com/ieeecsmuj', src: '/social_media/twitter.svg', alt: 'Twitter' }
+  ];
+
   return (
-    <section id='home' class='home'>
-      <div class='compass_grid'>
+    <section id="home" className="home">
+      <div className="compass_grid">
         <div></div>
-        <img src='/home/interactive_radial_pattern.svg' width='650px'/>
+        <img src="/home/interactive_radial_pattern.svg" alt="Radial Pattern" />
         <div></div>
       </div>
-      <div class='home_heading'>HACKERZSTREET</div>
-      <div class='home_subheading'>A 24-HOUR FLAGSHIP HACKATHON</div>
-      <div class='home_subheading'>BY IEEE CS MUJ</div>
-      <div class='home_grid'>
-        <div class='home_left'>
-          <div class='left_1'>3.0</div>
-          <div class='left_2'>Hackerzstreet 3.0, set to ignite creativity and innovation - an intense 24-hour coding marathon packed with excitement and collaboration. This event features a single round where participants delve into diverse technical themes, each presenting unique challenges and opportunities.</div>
-          <div class='left_3'>
-            <div class='left_3_1'>
+      <div className="home_heading"><div>HACKERZ</div><div>STREET</div></div>
+      <div className="home_subheading">A 24-HOUR FLAGSHIP HACKATHON</div>
+      <div className="home_subheading">BY IEEE CS MUJ</div>
+      <div className="home_grid">
+        <div className="home_left">
+          <div className="left_1">3.0</div>
+          <div className="left_2">
+            Hackerzstreet 3.0, set to ignite creativity and innovation - an intense 24-hour coding marathon packed with excitement and collaboration. This event features a single round where participants delve into diverse technical themes, each presenting unique challenges and opportunities.
+          </div>
+          <div className="left_3">
+            <div className="left_3_1">
               <div>Want to know more?</div>
               <p>Scroll to find out!</p>
             </div>
-            <div class='left_3_2'>
-              <a href='#schedule'><img src='/home/home_down_arrow.svg' width={15} /></a>
+            <div className="left_3_2">
+              <a href="#schedule">
+                <img src="/home/home_down_arrow.svg" alt="Down Arrow" />
+              </a>
             </div>
           </div>
         </div>
-        <div></div>
-        <div></div>
-        <div class='home_right'>
-          <div class='right_1'>
+        <div className='home_void'></div>
+        <div className='home_void'></div>
+        <div className="home_right">
+          <div className="right_1">
             <div>60K</div>
             <p>PRIZE POOL</p>
           </div>
-          <div class='right_2'>
-            <div class="fraction">
-              <div class='numerator_wrapper'>
-                <div class="numerator">12</div>
-                <p class="numerator">-</p>
+          <div className="right_2">
+            <div className="fraction">
+              <div className="numerator_wrapper">
+                <div className="numerator">12</div>
+                <p className="numerator">-</p>
               </div>
-              <hr></hr>
-              <div class='denominator_wrapper'>
-                <p class="denominator">-</p>
-                <div class="denominator">13</div>
+              <hr />
+              <div className="denominator_wrapper">
+                <p className="denominator">-</p>
+                <div className="denominator">13</div>
               </div>
             </div>
-            <div class="right_date">APRIL 2025</div>
+            <div className="right_date">APRIL 2025</div>
           </div>
-          <div class='right_3'>
-            <div><a href='https://www.instagram.com/ieee_csmuj/'><img src='/social_media/instagram.svg'/></a></div>
-            <div><a href='https://www.linkedin.com/company/ieee-cs-muj'><img src='/social_media/linkedin.svg'/></a></div>
-            <div><a href='https://medium.com/@ieeecs'><img src='/social_media/medium.svg'/></a></div>
-            <div><a href='https://x.com/ieeecsmuj'><img src='/social_media/twitter.svg'/></a></div>
+          <div className="right_3">
+            {socialLinks.map((link, index) => (
+              <div key={index}>
+                <a href={link.href}>
+                  <img src={link.src} alt={link.alt} />
+                </a>
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
+
+Home.propTypes = {
+  heading: PropTypes.string,
+  subheading: PropTypes.string
+};
+
+Home.defaultProps = {
+  heading: 'HACKERZSTREET',
+  subheading: 'A 24-HOUR FLAGSHIP HACKATHON'
+};
